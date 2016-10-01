b87b3e0c75930fabf36744222be35efdd60a9291
Han Hyuk Cho <han@Hans-MacBook-Air.local>
2016-10-01 15:09:26 -0400

    geolocation

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -1,7 +1,24 @@
-var app = angular.module('app', []);
+var app = angular.module('app', ['ngCordova']);
 
-app.controller('MainCtrl', function($scope) {
-    $scope.hello = function() {
-     return "Hello World from Angular!";
+app.controller('MainCtrl', function($scope, $cordovaGeolocation) {
+    $scope.lat = null;
+    $scope.lon = null;
+    var posOptions = {timeout: 5000, enableHighAccuracy: false};
+    var watchOptions = {timeout: 5000, enableHighAccuracy: false};
+
+
+    $scope.updateCoords = function() {
+        $cordovaGeolocation
+            .getCurrentPosition(posOptions)
+            .then(function(position) {
+                $scope.lat = position.coords.latitude;
+                $scope.lon = position.coords.longitude;
+                $scope.pos = position;
+            })
+            .catch(function(err){
+                console.error(err);
+            })
     }
+
 });
+
