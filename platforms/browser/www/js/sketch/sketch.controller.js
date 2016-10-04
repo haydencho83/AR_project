f4506db449c6da24083b4f0de3cb50fd60605bb0
Joe Bosman <joseph.a.bosman@gmail.com>
2016-10-04 13:00:56 -0400

    Removed random html that broke camera feed

--- platforms/browser/www/js/sketch/sketch.controller.js
+++ platforms/browser/www/js/sketch/sketch.controller.js
@@ -2,4 +2,8 @@ app.controller('SketchCtrl', function($scope, SketchFactory) {
 
 	SketchFactory.sketch(window, document) 
 
+	$scope.saveCanvas = SketchFactory.saveCanvas
+
+	$scope.loadCanvas = SketchFactory.loadCanvas
+
 });
