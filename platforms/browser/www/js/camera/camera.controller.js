8c79428c3b877d7ca221e76c8eadfbad9ecf05e0
Han Hyuk Cho <han@Hans-MacBook-Air.local>
2016-10-05 17:30:36 -0400

    added-camera-preview-pt-1

--- platforms/browser/www/js/camera/camera.controller.js
+++ platforms/browser/www/js/camera/camera.controller.js
@@ -2,4 +2,15 @@ app.controller('CameraCtrl', function($scope, CameraFactory) {
 	console.log('here')
 	CameraFactory.streamCamera()
 
+
+	/**********************************************************************************
+
+	< CAMERA PREVIEW >
+	document.addEventListener('deviceready', function () {
+    // console.log(CameraPreview)
+    cp.init();
+  }, false);
+
+	**********************************************************************************/
+
 });
